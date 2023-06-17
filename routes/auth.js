0c792923011834367a307dd022fddaada1f92c71
Sushant <sushantmore09072000@gmail.com>
2023-06-17 23:17:22 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+
+const router = require("express").Router();
+
+router.get("/login-success", (req, res) => {
+    if (req.user) {
+        res.json({
+            success: true,
+            status: 200,
+            message: "Google login successful",
+            user: req.user
+        })
+    }
+
+})
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect(process.env.CLIENT_URL)
+})
+
+router.get("/login-failed", (req, res) => {
+    res.json({
+        success: false,
+        status: 401,
+        message: "Google login failed"
+    })
+})
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }))
+router.get("/google/callback", passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login-failed"
+}))
+
+router.get("/github", passport.authenticate("github", { scope: ["profile"] }))
+router.get("/github/callback", passport.authenticate("github", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login-failed"
+}))
+
+module.exports = router
