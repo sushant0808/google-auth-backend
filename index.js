1d0ad9a0719af2d83eeeadf233bf0b6dd47ec8eb
Sushant <sushantmore09072000@gmail.com>
2023-06-18 06:23:26 +0530

    misc

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }))
