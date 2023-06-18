3dfcd006c6e7cfa492aa831c544c2f060f2bc457
Sushant <sushantmore09072000@gmail.com>
2023-06-18 19:11:34 +0530

    misc

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use((req, res, next) => {
+    console.log("CALLLLLEDDDD")
     res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL); // Replace with your frontend URL
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
